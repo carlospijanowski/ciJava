7264a8706e608785fa988f98c71cd6a22b89309e
CARTAXO <ccartaxo@partner.superapp-mtz.com>
2024-08-02 22:49:32 -0300

    Initial commit

---
+++ src/main/java/br/com/seteideias/cijava/CiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.seteideias.cijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiJavaApplication.class, args);
+    }
+
+}
